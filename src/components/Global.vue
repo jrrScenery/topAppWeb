2e26cc19160dac9b23b297ded9c0d4b6adfc1159
hedehu <okflash@126.com>
2018-07-31 16:38:00 +0800

    back

--- src/components/Global.vue
+++ src/components/Global.vue
@@ -2,8 +2,8 @@
 // const Server='http://localhost:8081';
 //const Server='http://47.104.200.60/api/proxy';
 //const Server='http://47.104.236.209:8084';
-// const Server='http://139.129.207.35:8084'
-const Server='http://192.168.0.105:8081'
+const Server='http://139.129.207.35:8084'
+// const Server='http://192.168.0.105:8081'
 const proxyServer=Server+'/api/proxy';
 let empId = "";
 let userInfo = [];
