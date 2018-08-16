fbb31c94e05dbc8043a8319b9c7b4fd42649487a
jingrr <jingrr@dcits.com>
2018-08-16 16:38:34 +0800

    new

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -76,11 +76,13 @@
               :min-width="item.width">
               <template slot-scope="scope">
                 <template v-if="item.prop == 'NOW_COLOR'">
+                //1 #ff0000 红，2 #ff9900 橙，3 #ffff00 黄，4 #009900 绿
                   <div>
                     <i v-if="scope.row[item.prop] == 0" style="display: inline-block; width: 0.14rem; height: 0.07rem; border-radius: 0.035rem;"></i>
                     <i v-if="scope.row[item.prop] == 1" style="display: inline-block; width: 0.14rem; height: 0.07rem; border-radius: 0.035rem; background: #ff0000;"></i>
                     <i v-if="scope.row[item.prop] == 2" style="display: inline-block; width: 0.14rem; height: 0.07rem; border-radius: 0.035rem; background: #ff9900;"></i>
-                    <i v-if="scope.row[item.prop] == 3" style="display: inline-block; width: 0.14rem; height: 0.07rem; border-radius: 0.035rem; background: #009900;"></i>
+                    <i v-if="scope.row[item.prop] == 3" style="display: inline-block; width: 0.14rem; height: 0.07rem; border-radius: 0.035rem; background: #ffff00;"></i>
+                    <i v-if="scope.row[item.prop] == 4" style="display: inline-block; width: 0.14rem; height: 0.07rem; border-radius: 0.035rem; background: #009900;"></i>
                   </div>
                 </template>
                 <router-link v-else :to="{name:'programShow',query:{projectId:scope.row['PROJECT_ID']}}">
