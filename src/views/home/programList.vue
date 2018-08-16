2f16e85b845fb1975d99989c05002dec2de43a3e
梁瑛 <liangyinga@dcits.com>
2018-08-16 14:39:30 +0800

    修正项目健康度的显示

--- src/views/home/programList.vue
+++ src/views/home/programList.vue
@@ -13,14 +13,18 @@
               </el-col>
               <el-col :span="9">
                 <div class="cellTopColor">
+                 //1 #ff0000 红，2 #ff9900 橙，3 #ffff00 黄，4 #009900 绿
                   <span v-if="item.BASE_COLOR == 0"></span>
                   <span v-if="item.BASE_COLOR == 1" style="background: #ff0000"></span>
-                  <span v-if="item.BASE_COLOR == 2" style="background: #ffff00"></span>
-                  <span v-if="item.BASE_COLOR == 3" style="background: #009900"></span>{{item.HEALTH_BASE_VALUE}}
+                  <span v-if="item.BASE_COLOR == 2" style="background: #ff9900"></span>
+                  <span v-if="item.BASE_COLOR == 3" style="background: #ffff00"></span>
+                  <span v-if="item.BASE_COLOR == 4" style="background: #009900"></span>{{item.HEALTH_BASE_VALUE}}
                   <span v-if="item.NOW_COLOR == 0"></span>
                   <span v-if="item.NOW_COLOR == 1" style="background: #ff0000"></span>
-                  <span v-if="item.NOW_COLOR == 2" style="background: #ffff00"></span>
-                  <span v-if="item.NOW_COLOR == 3" style="background: #009900"></span>{{item.HEALTH_CURRENT_VALUE}}
+                  <span v-if="item.NOW_COLOR == 2" style="background: #ff9900"></span>
+                  <span v-if="item.NOW_COLOR == 3" style="background: #ffff00"></span>
+                  <span v-if="item.NOW_COLOR == " st4yle="background: #009900"></span>{{item.HEALTH_CURRENT_VALUE}}
+
                 </div>
               </el-col>
               <el-col :span="7">
